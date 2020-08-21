905c02a614b58ef0d250cf9e5556398f36cc9be9
Envis10n <envis10n@protonmail.com>
2020-08-21 10:30:49 -0500

    Add update command.

---
+++ src/bot/commands/update.ts
@@ -0,0 +1,25 @@
+import { Command } from "discord-akairo";
+import { Message } from "discord.js";
+import { execAsync } from "../../lib/proc";
+
+class UpdateCommand extends Command {
+  constructor() {
+    super("update", {
+      aliases: ["update"],
+      ownerOnly: true,
+    });
+  }
+  async exec(message: Message) {
+    const msg: Message = await message.channel.send("Updating...");
+    try {
+      const res = await execAsync("git pull", { cwd: process.cwd() });
+      if (res.error != null)
+        await msg.edit(`\`\`\`\n${res.stderr}\n${res.stdout}\n\`\`\``);
+      else await msg.edit("Done.");
+    } catch (e) {
+      await msg.edit(`Error\`\`\`\n${e.message}\n\`\`\``);
+    }
+  }
+}
+
+export default UpdateCommand;
