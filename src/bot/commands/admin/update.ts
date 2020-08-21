aa25a0315094cc9d9012d3a44cb0522af5e620ff
Envis10n <envis10n@protonmail.com>
2020-08-21 11:21:55 -0500

    Update command folder structure.

--- src/bot/commands/admin/update.ts
+++ src/bot/commands/admin/update.ts
@@ -1,6 +1,6 @@
 import { Command } from "discord-akairo";
 import { Message } from "discord.js";
-import { execAsync } from "../../lib/proc";
+import { execAsync } from "../../../lib/proc";
 
 class UpdateCommand extends Command {
   constructor() {
