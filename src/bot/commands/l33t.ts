074d58d229558b146836343e4531246c433144e5
Andrew <envis10n@protonmail.com>
2020-08-21 15:23:18 -0500

    bring to latest (#1)
    * Move admin functions to separate file.
    Add flag to rebuild FULLY
    
    * Create l33t command
    
    Translates message to l33t speak
    
    Co-authored-by: Mike Cluck <mike-cluck@pluralsight.com>
    Co-authored-by: Mike Cluck <mcluck90@gmail.com>

---
+++ src/bot/commands/l33t.ts
@@ -0,0 +1,40 @@
+import { Command } from "discord-akairo";
+import { Message } from "discord.js";
+
+interface L33tArguments {
+  msg: string;
+}
+
+class L33TCommand extends Command {
+  constructor() {
+    super("l33t", {
+      aliases: ["leet"],
+      args: [
+        {
+          id: "msg",
+          type: "string",
+        },
+      ],
+    });
+  }
+
+  exec(message: Message, args: L33tArguments) {
+    const msg = args.msg
+      .replace(/the/gi, "t3h")
+      .replace(/owned/gi, "pwn3d")
+      .replace(/a/g, "@")
+      .replace(/A/g, "4")
+      .replace(/B/g, "8")
+      .replace(/c/gi, "(")
+      .replace(/e/gi, "3")
+      .replace(/f/gi, "ph")
+      .replace(/H/g, "#")
+      .replace(/i/gi, "1")
+      .replace(/o/gi, "0")
+      .replace(/T/g, "7")
+      .replace(/S/g, "5");
+    return message.reply(msg);
+  }
+}
+
+export default L33TCommand;
