07ab3b1a8061d15c6eac8789127bb18abccbd3e0
Envis10n <envis10n@protonmail.com>
2020-08-22 16:42:17 -0500

    Adjustments I guess?

--- src/lib/interpret.ts
+++ src/lib/interpret.ts
@@ -33,39 +33,44 @@ export declare interface InterpreterProcess {
 
 export const processes: Map<string, InterpreterProcess> = new Map();
 
-setInterval(async () => {
-  const dead: string[] = [];
-  for (const key of processes.keys()) {
-    const proc = processes.get(key);
-    if (proc == undefined) return;
-    try {
-      const res = await pidusage(proc.pid);
-      if (!proc.alive) {
-        console.log(
-          `Process ${proc.pid} is marked as dead, but is still running.`,
-        );
-        await fkill(proc.pid);
-        console.log(`Process ${proc.pid} forcefully killed.`);
-      } else {
-        if (res.memory >= 150 * 1024 * 1024) {
-          // Too much memory, kill it
-          console.log(
-            `Process ${proc.pid} using too much memory. (${(
-              res.memory /
-              1024 /
-              1024
-            ).toFixed(2)} mb)`,
-          );
-          await fkill(proc.pid);
-          console.log(`Process ${proc.pid} forcefully killed.`);
+function interval() {
+  setTimeout(() => {
+    (async () => {
+      const dead: string[] = [];
+      for (const key of processes.keys()) {
+        const proc = processes.get(key);
+        if (proc == undefined) return;
+        try {
+          const res = await pidusage(proc.pid);
+          if (!proc.alive) {
+            console.log(
+              `Process ${proc.pid} is marked as dead, but is still running.`,
+            );
+            await fkill(proc.pid);
+            console.log(`Process ${proc.pid} forcefully killed.`);
+          } else {
+            if (res.memory >= 150 * 1024) {
+              // Too much memory, kill it
+              console.log(
+                `Process ${proc.pid} using too much memory. (${(
+                  res.memory / 1024
+                ).toFixed(2)} mb)`,
+              );
+              await fkill(proc.pid);
+              console.log(`Process ${proc.pid} forcefully killed.`);
+            }
+          }
+        } catch (e) {
+          // Can't get process info, remove it.
+          dead.push(key);
         }
       }
-    } catch (e) {
-      // Can't get process info, remove it.
-      dead.push(key);
-    }
-  }
-  dead.forEach((k) => {
-    processes.delete(k);
-  });
-}, 1000);
+      dead.forEach((k) => {
+        processes.delete(k);
+      });
+    })().then(() => {
+      interval();
+    });
+  }, 1000);
+}
+interval();
