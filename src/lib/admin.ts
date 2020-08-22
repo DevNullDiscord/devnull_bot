c1ada57e3b2884a49ee94a2379936e8b35f9d9ff
Envis10n <envis10n@protonmail.com>
2020-08-21 20:47:08 -0500

    Back to old reload behavior.

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -42,25 +42,9 @@ export async function rebuild(
 
 export async function reload(this: Command, message: Message) {
   const msg: Message = await message.channel.send("Reloading...");
-  this.client.commandHandler = new CommandHandler(this.client, {
-    directory: Path.resolve(__dirname, "../bot/commands/"),
-    prefix: cmdPrefix,
-    defaultCooldown: 1000,
-    allowMention: true,
-    ignoreCooldown: ownerID,
-    ignorePermissions: ownerID,
-  });
-  this.client.listenerHandler = new ListenerHandler(this.client, {
-    directory: Path.resolve(__dirname, "../bot/listeners/"),
-  });
-  this.client.inhibitorHandler = new InhibitorHandler(this.client, {
-    directory: Path.resolve(__dirname, "../bot/inhibitors/"),
-  });
-  this.client.commandHandler.useListenerHandler(this.client.listenerHandler);
-  this.client.commandHandler.useInhibitorHandler(this.client.inhibitorHandler);
-  this.client.listenerHandler.loadAll();
-  this.client.inhibitorHandler.loadAll();
-  this.client.commandHandler.loadAll();
+  this.client.listenerHandler?.reloadAll();
+  this.client.inhibitorHandler?.reloadAll();
+  this.client.commandHandler?.reloadAll();
   await msg.edit("Reloading...\nDone.");
 }
 
