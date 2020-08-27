30b7d7960aa38b6bfb4e1e3b5115f53c61a78d80
Envis10n <envis10n@protonmail.com>
2020-08-27 10:58:14 -0500

    Switch temp to regex command Limit 1 every  10 seconds

--- src/bot/commands/temp.ts
+++ src/bot/commands/temp.ts
@@ -2,10 +2,35 @@ import { Command } from "discord-akairo";
 import { Message } from "discord.js";
 import { MessageEmbed } from "discord.js";
 
-interface TempArguments {
-  temp: number | null;
-  type: string;
+interface TempArgs {
+  match: RegExpMatchArray | null;
 }
+
+interface TempConverter {
+  c(temp: number): number;
+  f(temp: number): number;
+  k(temp: number): number;
+}
+
+const converters: { [key: string]: TempConverter } = {};
+
+converters["k"] = {
+  c: KelvinToCelcius,
+  f: KelvinToFarenheit,
+  k: (temp) => temp,
+};
+
+converters["c"] = {
+  c: (temp) => temp,
+  f: CelciusToFarenheit,
+  k: CelciusToKelvin,
+};
+
+converters["f"] = {
+  c: FarenheitToCelcius,
+  f: (temp) => temp,
+  k: FarenheitToKelvin,
+};
 
 function KelvinToCelcius(temp: number): number {
   return temp - 273.15;
@@ -31,67 +56,48 @@ function FarenheitToCelcius(temp: number): number {
   return (temp - 32) / (9 / 5);
 }
 
+let lastUse: number = 0;
+
 class TempCommand extends Command {
   constructor() {
     super("temp", {
-      aliases: ["temp"],
-      args: [
-        {
-          id: "temp",
-          type: "number",
-        },
-        {
-          id: "type",
-          type: "string",
-          default: "c",
-          match: "restContent",
-        },
-      ],
+      regex: /(\d+\.?\d+?)([kcfKCF])/i,
     });
   }
-
-  exec(message: Message, args: TempArguments) {
-    if (args.temp == null) {
-      message.reply(
-        "Please provide a temperature value.\nUsage: `temp [temp] <c|f|k>`",
-      );
-    } else {
-      let c: number;
-      let f: number;
-      let k: number;
-      let degree: string = "°";
-      const emb: MessageEmbed = new MessageEmbed();
-      emb.setTitle("Temperature Conversion");
-      switch (args.type.toLowerCase()) {
+  async exec(message: Message, args: TempArgs) {
+    if (args.match != null) {
+      if (Date.now() - lastUse < 10000) return;
+      const mTemp: string = args.match[1];
+      const mType: string = args.match[2].toLowerCase();
+      const tVal: number = parseFloat(mTemp);
+      if (isNaN(tVal) || !isFinite(tVal)) return;
+      const converter: TempConverter = converters[mType];
+      let reply: string = "";
+      switch (mType) {
         case "c":
-          // celcius
-          c = args.temp;
-          f = CelciusToFarenheit(c);
-          k = CelciusToKelvin(c);
-          emb.addField("Celcius", `${c.toFixed(2)}${degree}C`);
-          emb.addField("Farenheit", `${f.toFixed(2)}${degree}F`);
-          emb.addField("Kelvin", `${k.toFixed(2)}K`);
+          reply = `${tVal.toFixed(2)} degrees Celcius: ${converter
+            .f(tVal)
+            .toFixed(2)} degrees Farenheit, ${converter
+            .k(tVal)
+            .toFixed(2)} Kelvin`;
           break;
         case "f":
-          // farenheit
-          f = args.temp;
-          c = FarenheitToCelcius(f);
-          k = FarenheitToKelvin(f);
-          emb.addField("Farenheit", `${f.toFixed(2)}${degree}F`);
-          emb.addField("Celcius", `${c.toFixed(2)}${degree}C`);
-          emb.addField("Kelvin", `${k.toFixed(2)}K`);
+          reply = `${tVal.toFixed(2)} degrees Farenheit: ${converter
+            .c(tVal)
+            .toFixed(2)} degrees Celcius, ${converter
+            .k(tVal)
+            .toFixed(2)} Kelvin`;
           break;
         case "k":
-          // kelvin
-          k = args.temp;
-          c = KelvinToCelcius(k);
-          f = KelvinToFarenheit(k);
-          emb.addField("Kelvin", `${k.toFixed(2)}K`);
-          emb.addField("Celcius", `${c.toFixed(2)}${degree}C`);
-          emb.addField("Farenheit", `${f.toFixed(2)}${degree}F`);
+          reply = `${tVal.toFixed(2)} Kelvin: ${converter
+            .f(tVal)
+            .toFixed(2)} degrees Farenheit, ${converter
+            .c(tVal)
+            .toFixed(2)} degrees Celcius`;
           break;
       }
-      message.reply(emb);
+      await message.channel.send(reply);
+      lastUse = Date.now();
     }
   }
 }
