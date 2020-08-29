6eda0e003cd8dc7537e4aba945b24fa59fc766b7
Envis10n <envis10n@protonmail.com>
2020-08-29 12:27:00 -0500

    Oops

--- src/bot/commands/regex/arch.ts
+++ src/bot/commands/regex/arch.ts
@@ -3,7 +3,7 @@ import { Message } from "discord.js";
 
 class ArchCommand extends Command {
   constructor() {
-    super("temp", {
+    super("arch", {
       regex: /(?:arch linux|arch|manjaro|antergos)\b/i,
       category: "regex",
       cooldown: 60000 * 5,
