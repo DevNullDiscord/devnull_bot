9399b3aaa455ae36e916a4fc75a646c0782d0fe9
Envis10n <envis10n@protonmail.com>
2020-08-29 13:06:29 -0500

    fixx

--- src/bot/commands/regex/arch.ts
+++ src/bot/commands/regex/arch.ts
@@ -6,13 +6,13 @@ let lastTime: number = 0;
 class ArchCommand extends Command {
   constructor() {
     super("arch", {
-      regex: /(?:arch linux|arch|manjaro|antergos|endeavour|ᵃʳᶜʰ)\b/i,
+      regex: /(?:arch|manjaro|antergos|endeavour|ᵃʳᶜʰ)/i,
       category: "regex",
     });
   }
   exec(message: Message, args: { match: RegExpMatchArray }) {
     if (Date.now() - lastTime < 60000 * 15) return;
-    if (args.match[0].includes("ᵃʳᶜʰ")) message.channel.send("ᶦ ᵘˢᵉ ᵃʳᶜʰ ᵇᵗʷ");
+    if (args.match[0] == "ᵃʳᶜʰ") message.channel.send("ᶦ ᵘˢᵉ ᵃʳᶜʰ ᵇᵗʷ");
     else message.channel.send("*I use arch btw*");
     lastTime = Date.now();
   }
