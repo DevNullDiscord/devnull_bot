19bf981dcae99eeb2496d6442e60a119ae4f5ee4
Envis10n <envis10n@protonmail.com>
2020-08-29 13:02:21 -0500

    Adjust arch timeout

--- src/bot/commands/regex/arch.ts
+++ src/bot/commands/regex/arch.ts
@@ -1,17 +1,20 @@
 import { Command } from "discord-akairo";
 import { Message } from "discord.js";
 
+let lastTime: number = 0;
+
 class ArchCommand extends Command {
   constructor() {
     super("arch", {
-      regex: /(?:arch linux|arch|manjaro|antergos)\b/i,
+      regex: /(?:arch linux|arch|manjaro|antergos|endeavour|ᵃʳᶜʰ)\b/i,
       category: "regex",
-      cooldown: 60000 * 5,
-      ratelimit: 1,
     });
   }
-  exec(message: Message) {
-    message.channel.send("*I use arch btw*");
+  exec(message: Message, args: { match: RegExpMatchArray }) {
+    if (Date.now() - lastTime < 60000 * 15) return;
+    if (args.match[0].includes("ᵃʳᶜʰ")) message.channel.send("ᶦ ᵘˢᵉ ᵃʳᶜʰ ᵇᵗʷ");
+    else message.channel.send("*I use arch btw*");
+    lastTime = Date.now();
   }
 }
 
