dce02a4bad60aee20af22c0911b2bc679c0fa7fd
Envis10n <envis10n@protonmail.com>
2020-09-11 11:07:46 -0500

    Update storage system. Add storage data dir config var. Save storage on SIGINT shutdown.

--- src/storage.ts
+++ src/storage.ts
@@ -1,7 +1,60 @@
-interface IUserData {
-  id: string;
+import Path from "path";
+import fs from "fs-extra";
+import Discord from "discord.js";
+import { dataPath } from "./config";
+
+if (!fs.existsSync(dataPath)) {
+  fs.ensureDirSync(dataPath);
 }
 
-const storage: Map<string, IUserData> = new Map();
+let storageLock: boolean = false;
 
-export default storage;
+const storage: { [key: string]: any } = {};
+
+export async function storageWait() {
+  return new Promise((resolve, reject) => {
+    while (storageLock) {}
+    resolve();
+  });
+}
+
+export async function loadStorage() {
+  await storageWait();
+  storageLock = true;
+  for (const fname of await fs.readdir(dataPath)) {
+    const fpath = Path.resolve(dataPath, fname);
+    const finfo = Path.parse(fpath);
+    const fdata = await fs.readFile(fpath);
+    storage[finfo.name] = JSON.parse(fdata.toString());
+  }
+  storageLock = false;
+}
+
+export async function saveStorage() {
+  await storageWait();
+  storageLock = true;
+  for (const key in storage) {
+    const fpath = Path.resolve(dataPath, `${key}.json`);
+    const fdata = JSON.stringify(storage[key]);
+    await fs.writeFile(fpath, fdata);
+  }
+  storageLock = false;
+}
+
+/**
+ * Only use during shutdown! Will lock storage.
+ */
+export function saveSync() {
+  while (storageLock) {}
+  storageLock = true;
+  for (const key in storage) {
+    const fpath = Path.resolve(dataPath, `${key}.json`);
+    const fdata = JSON.stringify(storage[key]);
+    fs.writeFileSync(fpath, fdata);
+  }
+}
+
+export function getCollection<T>(name: string): { [key: string]: T } {
+  if (storage[name] == undefined) storage[name] = {};
+  return storage[name] as { [key: string]: T };
+}
