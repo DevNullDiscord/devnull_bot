b481e64e55143969b15d11f86595186ef592a34e
Mike Cluck <mike-cluck@pluralsight.com>
2020-09-12 16:13:44 -0600

    Remove unnecessary file lock

--- src/storage.ts
+++ src/storage.ts
@@ -7,47 +7,30 @@ if (!fs.existsSync(dataPath)) {
   fs.ensureDirSync(dataPath);
 }
 
-let storageLock: boolean = false;
-
 const storage: { [key: string]: any } = {};
 
-export async function storageWait() {
-  return new Promise((resolve, reject) => {
-    while (storageLock) {}
-    resolve();
-  });
-}
-
 export async function loadStorage() {
-  await storageWait();
-  storageLock = true;
   for (const fname of await fs.readdir(dataPath)) {
     const fpath = Path.resolve(dataPath, fname);
     const finfo = Path.parse(fpath);
     const fdata = await fs.readFile(fpath);
     storage[finfo.name] = JSON.parse(fdata.toString());
   }
-  storageLock = false;
 }
 
 export async function saveStorage() {
-  await storageWait();
-  storageLock = true;
-  for (const key in storage) {
+  for (const key of Object.keys(storage)) {
     const fpath = Path.resolve(dataPath, `${key}.json`);
     const fdata = JSON.stringify(storage[key]);
     await fs.writeFile(fpath, fdata);
   }
-  storageLock = false;
 }
 
 /**
- * Only use during shutdown! Will lock storage.
+ * Only use during shutdown!
  */
 export function saveSync() {
-  while (storageLock) {}
-  storageLock = true;
-  for (const key in storage) {
+  for (const key of Object.keys(storage)) {
     const fpath = Path.resolve(dataPath, `${key}.json`);
     const fdata = JSON.stringify(storage[key]);
     fs.writeFileSync(fpath, fdata);
@@ -55,6 +38,6 @@ export function saveSync() {
 }
 
 export function getCollection<T>(name: string): { [key: string]: T } {
-  if (storage[name] == undefined) storage[name] = {};
+  if (storage[name] === undefined) storage[name] = {};
   return storage[name] as { [key: string]: T };
 }
