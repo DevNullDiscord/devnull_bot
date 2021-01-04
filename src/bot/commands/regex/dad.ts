2c3d500a646d533379be259246065141cc1a1da9
Envis10n <envis10n@protonmail.com>
2021-01-04 11:34:54 -0600

    Lol first pass dad bot

---
+++ src/bot/commands/regex/dad.ts
@@ -0,0 +1,21 @@
+import { Command } from "discord-akairo";
+import { Message } from "discord.js";
+
+interface IDadReg {
+  match: RegExpMatchArray;
+}
+
+class DadBotCommand extends Command {
+  constructor() {
+    super("dadbot", {
+      regex: /\b(?:i am|i'm)\b(.*)(?:[.?!]|\n)?/i,
+      category: "regex",
+    });
+  }
+  exec(message: Message, args: IDadReg) {
+    const sub: string = args.match[1].trim();
+    message.util!.reply(`Hi ${sub}, I'm dad.`);
+  }
+}
+
+export default DadBotCommand;
